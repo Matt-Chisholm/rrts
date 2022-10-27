7abe3ecfc0372fdfc6fce52ad7d7c36bbbe87696
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-10-27 09:55:52 -0700

    simple counter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-export default class App extends Component {
+interface AppProps {
+  color?: string;
+}
+
+export default class App extends Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App color="red" />, document.getElementById('root'));
