90a8b43967e04c5b54b9f6ca88115f67fb4e98b1
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-11-12 11:23:08 -0800

    todo interface

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
 import { Dispatch } from "redux";
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 export const fetchTodos = () => {
     return async (dispatch: Dispatch) => {
-        const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+        const response = await axios.get<Todo[]>("https://jsonplaceholder.typicode.com/todos");
 
         dispatch({
             type: "FETCH_TODOS",
